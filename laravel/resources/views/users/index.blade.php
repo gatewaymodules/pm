ce7557bfd2a939b1429338c713609156e6d34004
Eugene <eugenevdm@gmail.com>
2015-06-19 19:45:07 +0200

    Misc changes.

--- laravel/resources/views/users/index.blade.php
+++ laravel/resources/views/users/index.blade.php
@@ -5,7 +5,7 @@
     <h1>All Users</h1>
 
     <p>{!! link_to_route('users.create', 'Register new user') !!}</p>
-    <p><a href="{{ url('auth/logout') }}">Register</a></p>
+    <p><a href="{{ url('auth/logout') }}">Logout</a></p>
 
     @if ($users->count())
         <table class="table table-striped table-bordered">
@@ -20,11 +20,9 @@
             <tbody>
             @foreach ($users as $user)
                 <tr>
-
                     <td>{{ $user->name }}</td>
                     <td>{{ $user->email }}</td>
                     <td>{{ $user->phone }}</td>
-
                     <td>{!! link_to_route('users.edit', 'Edit', array($user->id), array('class' => 'btn btn-info')) !!}</td>
                     <td>
                         {!! Form::open(array('method'=> 'DELETE', 'route' => array('users.destroy', $user->id))) !!}
